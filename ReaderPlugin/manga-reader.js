6aaf97a4674498f6e92f110d193cc34da77abee1
Sykhang Ha <31758072+sykhangdha@users.noreply.github.com>
2023-02-13 00:58:07 -0800

    Update manga-reader.js
    Added click on image function to script for mobile users

--- ReaderPlugin/manga-reader.js
+++ ReaderPlugin/manga-reader.js
@@ -1,40 +1,30 @@
 jQuery(document).ready(function($) {
-    // Get the manga reader container
-    var mangaContainer = $('.manga-reader');
+  $('.manga-reader').each(function() {
+    var $reader = $(this),
+        $pages = $reader.find('.manga-page'),
+        currentIndex = 0;
 
-    // Get all the manga pages
-    var mangaPages = mangaContainer.find('.manga-page');
-
-    // Set the current page to the first one
-    var currentPage = 0;
-
-    // Hide all the pages except for the first one
-    mangaPages.not(':eq(0)').hide();
-
-    // Handle arrow key presses
-    $(document).keydown(function(e) {
-        switch (e.which) {
-            case 37: // Left arrow
-                if (currentPage > 0) {
-                    mangaPages.eq(currentPage).hide();
-                    currentPage--;
-                    mangaPages.eq(currentPage).show();
-                }
-                break;
-
-            case 39: // Right arrow
-                if (currentPage < mangaPages.length - 1) {
-                    mangaPages.eq(currentPage).hide();
-                    currentPage++;
-                    mangaPages.eq(currentPage).show();
-                }
-                break;
-
-            default:
-                return;
-        }
-
-        e.preventDefault();
+    $reader.on('click', '.manga-page', function() {
+      currentIndex = ($pages.index(this) + 1) % $pages.length;
+      $pages.hide();
+      $pages.eq(currentIndex).show();
     });
-});
 
+    $(document).keydown(function(e) {
+      switch(e.which) {
+        case 37: // left
+          currentIndex = (currentIndex + $pages.length - 1) % $pages.length;
+          break;
+        case 39: // right
+          currentIndex = (currentIndex + 1) % $pages.length;
+          break;
+        default: return;
+      }
+      $pages.hide();
+      $pages.eq(currentIndex).show();
+      e.preventDefault();
+    });
+
+    $pages.eq(0).show();
+  });
+});
